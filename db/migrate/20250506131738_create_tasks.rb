493eadd4f158fd17c3f4340520cf8c4bde2d90d3
Mariana Barbieru <barbieru_mariana@yahoo.com>
2025-05-06 17:07:04 +0100

    laundry

---
+++ db/migrate/20250506131738_create_tasks.rb
@@ -0,0 +1,13 @@
+class CreateTasks < ActiveRecord::Migration[7.1]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.string :string
+      t.string :details
+      t.string :text
+      t.boolean :completed
+
+      t.timestamps
+    end
+  end
+end
